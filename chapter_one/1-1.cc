15a2be3c8f99958cbd12ee1cb7a36f046205a74a
fresto32 <tajpereira@gmail.com>
2018-02-12 17:44:08 +1030

    1.1

--- chapter_one/1-1.cc
+++ chapter_one/1-1.cc
@@ -1,32 +1,14 @@
-int partition(char *str_to_part, int low, int high) {
-  int first_high = 0;
-  int p = high;
+#include <bitset>
+#include <string>
 
-  for (int i=0; *(str_to_part+i); i++) {
-    if(*(str_to_part + high) > *(str_to_part+i)) {
-      swap(*(str_to_part+i),*(str_to_part+first_high));
-      first_high++;
-    }
+using namespace std;
+
+bool string_hax_unique_BS(string stu) {
+  bitset<256> bs_chars(0);
+
+  for (int i = 0; i < stu.length(); i++) {
+    if (bs_chars.test((int) stu[i] - 'a')) return false;
+    else bs_chars.set((int) stu[i] - 'a');
   }
-
-  swap(*(str_to_part+high), *(str_to_part+first_high));
-  return first_high;
-}
-
-void quicksort(char *str_to_sort, int low, int high) {
-  if (*str_to_sort) return;
-  else if (low < high) {
-    int p = partition(str_to_sort, low, high);
-    quicksort(str_to_sort, low, p-1);
-    quicksort(str_to_sort, p+1, high);
-  }
-}
-
-bool unique_string(char *str_to_unique) {
-  quicksort(str_to_unique, 0, *str_to_unique.size()-1);
-  
-  for (char *it = str_to_unique; *it; ++it)
-    if (*it == *(it+1)) return false;
   return true;
 }
-
